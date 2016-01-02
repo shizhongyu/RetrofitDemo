cdfb288969b839381ecb329e7db40be4485ce996
lizhongyu <1966957866@qq.com>
2016-01-02 13:26:21 -0800

    okhttp

--- app/src/main/java/com/example/zhongyu/retrofitdemo/MainActivity.java
+++ app/src/main/java/com/example/zhongyu/retrofitdemo/MainActivity.java
@@ -2,47 +2,35 @@ package com.example.zhongyu.retrofitdemo;
 
 import android.app.Activity;
 import android.os.Bundle;
-import java.io.IOException;
-import java.util.List;
 
-import retrofit.Call;
-import retrofit.Retrofit;
 
 /**
  * Created by zhongyu on 1/2/2016.
  */
 public class MainActivity extends Activity{
+    private static final String TAG = "MainActivity";
 
     public static final String API_URL = "https://api.github.com";
+
+    OkhttpUtils okhttpUtils = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        try {
-            getApi();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        init();
+        okHttptest();
     }
 
-    public void getApi() throws IOException {
-        // Create a very simple REST adapter which points the GitHub API.
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(API_URL)
-                .build();
 
-        // Create an instance of our GitHub API interface.
-        GitHub github = retrofit.create(GitHub.class);
+    public void init(){
+        okhttpUtils = new OkhttpUtils();
+    }
 
-        // Create a call instance for looking up Retrofit contributors.
-        Call<List<Contributor>> call = github.contributors("square", "retrofit");
+    private void okHttptest(){
+//        okhttpUtils.getOkhttp();
 
-        // Fetch and print a list of the contributors to the library.
-        List<Contributor> contributors = call.execute().body();
-        for (Contributor contributor : contributors) {
-            System.out.println(contributor.login + " (" + contributor.contributions + ")");
-        }
+        okhttpUtils.postOkhttpRequest();
     }
 
 }
---
+++ app/src/main/java/com/example/zhongyu/retrofitdemo/OkhttpUtils.java
@@ -0,0 +1,98 @@
+package com.example.zhongyu.retrofitdemo;
+
+import android.app.DownloadManager;
+import android.provider.MediaStore;
+import android.util.Log;
+
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
+/**
+ * Created by zhongyu on 1/2/2016.
+ */
+public class OkhttpUtils {
+    private static final String TAG = "OkhttpUtils";
+
+    OkHttpClient okHttpClient = null;
+    Response response = null;
+    public static final String ENDPOINT = "https://api.github.com/repos/square/okhttp/contributors";
+    public static final String KEY_VALUE_POST = "https://en.wikipedia.org/w/index.php";
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    public static final MediaType MEDIA_TYPE_MARKDOWN = MediaType.parse("text/x-markdown; charset=utf-8");
+
+    public void getOkhttp() {
+        new Thread(new Runnable() {
+            OkHttpClient okHttpClient = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(ENDPOINT)
+                    .build();
+            @Override
+            public void run() {
+                try {
+                    Response response = okHttpClient.newCall(request).execute();
+                    Log.d(TAG, "run() called with: " + response.body().toString());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    public void postOkhttp(final String url,final String json){
+        new Thread(new Runnable() {
+            OkHttpClient okHttpClient = new OkHttpClient();
+            RequestBody requestBody = RequestBody.create(JSON,json);
+            Request request = new Request.Builder()
+                    .url(url)
+                    .post(requestBody)
+                    .build();
+            @Override
+            public void run() {
+                try {
+                    Response response =okHttpClient.newCall(request).execute();
+                    Log.d(TAG, "run() called with: " + response.body().toString());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public void postOkhttpRequest(){
+        new Thread(new Runnable() {
+            OkHttpClient okHttpClient = new OkHttpClient();
+            RequestBody requestBody = new FormEncodingBuilder()
+                    .add("search","Jurassic park")
+                    .build();
+
+            Request request = new Request.Builder()
+                    .url(KEY_VALUE_POST)
+                    .post(requestBody)
+                    .build();
+            @Override
+            public void run() {
+                try {
+                    Response response = okHttpClient.newCall(request).execute();
+                    Log.d(TAG, "postOkhttpRequest() called with: " + response.body().toString());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+    }
+}
+
+
+
+
+
+
